1396f4a8278e9d30ba31592d5829612c567fb44d
Nevin Herring <nevin.herring@gmail.com>
2021-02-15 08:28:30 -0600

    added more bitwise operators

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@ namespace BitwiseAndShiftOperators
             WriteLine($"a & b = {a & b}");  // 2-bit column only
             WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns
             WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columnss
+            // 0101 0000 left-shift a by three bit columns
+            WriteLine($"a << 3 = {a << 3}");
+            // multiply a by 8
+            WriteLine($"a * 8 = {a * 8}");
+            // 0000 0011 right-shift b by one bit column
+            WriteLine($"b >> 1 = {b >> 1}");
         }
     }
 }
