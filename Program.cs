39a9ca8da790ece92238a968d7e77b2d3ec6a26b
Nevin Herring <nevin.herring@gmail.com>
2021-02-15 08:23:43 -0600

    commit prior to testing

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace BitwiseAndShiftOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; // 0000 1010
+            int b = 6;  // 0000 0110
+            WriteLine($"a = {a}");
+            WriteLine($"b = {b}");
+            WriteLine($"a & b = {a & b}");  // 2-bit column only
+            WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns
+            WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columnss
+        }
+    }
+}
